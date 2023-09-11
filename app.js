423d5e1c3a69a98d27a3c3c19b220cf034ed7e2d
yudistiraardi <mail.ardix@gmail.com>
2023-09-11 10:00:27 +0700

    Update codes

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 let count = 0;
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
 
-const value = document.getElementById('value');
-const btns = document.querySelectorAll('.btn');
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (ev) {
+    const click = ev.currentTarget.classList;
 
-btns.forEach(function(btn) {
+    if (click.contains("decrease")) {
+      count--;
+    } else if (click.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
 
-    console.log(btns);
+    if (count < 0) {
+      value.style.color = "rgb(" + Math.abs(count) * 30 + ", 0, 0)";
+    } else if (count > 0) {
+      value.style.color = "rgb(0, " + count * 30 + ", 0)";
+    } else {
+      value.style.color = "black";
+    }
 
-    btn.addEventListener('click', function(ev){
-
-        console.log(ev.currentTarget.classList);
-        const click = ev.currentTarget.classList;
-        if (click.contains('decrease')) {
-            count--;
-        }
-        else if (click.contains('increase')) {
-            count++;
-        }
-        else {
-            count = 0;
-        }
-        if (count < 0) {
-            value.style.color = 'rgb('+(Math.abs(count) * 30)+', 0, 0)';
-        } else if (count > 0) {
-            value.style.color = 'rgb(0, '+(count * 30)+', 0)';
-        } else {
-            value.style.color = 'black';
-        }
-        value.textContent = count;
-    });
+    value.textContent = count;
+  });
 });
